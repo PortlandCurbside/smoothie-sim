07e20d34c55ad8a1527df2bd37802872554c17c8
Chris Evans <cwevans612@gmail.com>
2023-04-01 12:08:22 -0400

    test

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name:"SmoothieSim",
-            script:"(cd smoothie-sim; npm run start)"
+            script:"cd smoothie-sim; npm run start"
         }
     ]
 }
