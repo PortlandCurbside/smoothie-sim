f52de4f7bf6b1eca223d323d0b7168bd7ba7ec86
Kyle Provencal <kjprovencal@gmail.com>
2023-04-01 11:13:21 -0400

    Created mongoose client connection promise module

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   experimental: {
     appDir: true,
+    serverComponentsExternalPackages: ['mongoose']
   },
+  webpack(config) {
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    return config;
+  } 
 }
 
 module.exports = nextConfig
