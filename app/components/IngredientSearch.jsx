0352b647414ca5551ebae7ba55b6c39e7dc65074
Chris Evans <cwevans612@gmail.com>
2023-04-02 01:39:19 -0400

    card added

--- app/components/IngredientSearch.jsx
+++ app/components/IngredientSearch.jsx
@@ -3,12 +3,13 @@ export default function IngredientSearch(props){
     return (
         <div className={`
             transition-all
+            rounded-lg
             h-64
             w-full
             md:w-3/4
             xl:w-full
             xl:grow
-            bg-amber-400 text-center
+            bg-amber text-center
         `}>
             <p> Put Ingredients Search Here </p>
         </div>
