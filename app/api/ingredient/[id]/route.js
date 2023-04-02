653034f463b50b1f356ba73466abdfccb29389b5
Kyle Provencal <kjprovencal@gmail.com>
2023-04-01 22:11:22 -0400

    Added path aliases

--- app/api/ingredient/[id]/route.js
+++ app/api/ingredient/[id]/route.js
@@ -1,4 +1,4 @@
-import clientPromise from "../../lib/mongodb"
+import clientPromise from "@/lib/mongodb"
 import { NextResponse } from "next/server"
 
 export async function GET(request, { params }) {
