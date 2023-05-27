c505f95fd69ec6aa4b40b92490c865ac6b28dbe6
Chris Evans <cwevans612@gmail.com>
2023-05-27 10:37:36 -0400

    playing around with intersection

--- app/components/SmoothieInside.jsx
+++ app/components/SmoothieInside.jsx
@@ -3,7 +3,7 @@
 import { MeshWobbleMaterial, useGLTF  } from "@react-three/drei"
 import { useFrame, useThree } from "@react-three/fiber"
 import { useEffect, useRef } from "react"
-import { DoubleSide, DataTexture, RedFormat, LuminanceFormat, Vector3, CylinderGeometry } from 'three'
+import { DoubleSide, DataTexture, RedFormat, LuminanceFormat, Vector3, CylinderGeometry, BufferAttribute } from 'three'
 
 
 function vortex(x, y, depth, diameter){
@@ -38,18 +38,42 @@ export default function SmoothieInside(props) {
         colors[ c ] = ( c / colors.length ) * 256;
     }
 
+    // These are used for both making the cylinder and calculating its boundary
+    var cyl_toprad = 5;
+    var cyl_botrad = 3;
+    var cyl_height = 10;
+
+    // takes a Vector3
+    const outsideOfCylinder = (surface_point) => {
+        let z = surface_point.z;
+
+        let surface_point_rad = Math.sqrt(Math.pow(surface_point.x, 2), Math.pow(surface_point.y, 2))
+        let acceptable_rad = 2*z
+
+        return surface_point_rad > acceptable_rad
+
+    }
+
     const gradientMap = new DataTexture( colors, colors.length, 1, format );
     gradientMap.needsUpdate = true;
 
 
-    useFrame(( { clock } ) => {
-        // generate Sin matrix and matrix transform it
-        const count = plane.current.attributes.position.count
-        const damping = 0.75
+
+    if(plane.attributes){
+        console.log("INITIATING Buffer Attrib")
+        const surface_count = plane.getAttribute(position).count
+        let visible = new Array(surface_count).fill(1)
+        plane.attributes.setAttribute('visible', BufferAttribute(visible, 1))
+    }
+
+    useFrame(( { clock } ) => { // generate Sin matrix and matrix transform it
+        const surface_count = plane.current.attributes.position.count
+
+        const damping = 0.74
 
         const time = clock.getElapsedTime();
 
-        for(let i=0; i<count; i++){
+        for(let i=0; i<surface_count; i++){
             // Current point
             let x = plane.current.attributes.position.getX(i);
             let y = plane.current.attributes.position.getY(i);
@@ -78,7 +102,13 @@ export default function SmoothieInside(props) {
         
             //plane.current.attributes.position.setZ(i, new_z+vortex(x, y, depth, diameter))
             plane.current.attributes.position.setXYZ(i, point.x, point.y, point.z)
+            //if(outsideOfCylinder(point)){
+             //   plane.current.attributes.visible.array[i] = 0
+            //}else{
+                //plane.current.attributes.visible.array[i] = 1
+            //}
         }
+        //plane.current.attributes.visible.needsUpdate=true
         plane.current.computeVertexNormals()
         plane.current.attributes.position.needsUpdate=true
 
@@ -87,7 +117,7 @@ export default function SmoothieInside(props) {
     return(
         <>
         <mesh position={[0, -4, 0]}>
-            <cylinderGeometry args={[5,  3,  10, 128, 10, true, 0, 2*Math.PI]}/>
+            <cylinderGeometry args={[cyl_toprad, cyl_botrad, cyl_height, 128, 10, true, 0, 2*Math.PI]}/>
             <meshToonMaterial side={DoubleSide} receiveShadow castShadow
                 color={"#FF6978"} gradientMap={gradientMap} wireframe={false} />
         </mesh>
